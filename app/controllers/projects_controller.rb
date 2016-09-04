fc6ccf0626ffd5c088a967316ec2a8e05c632c2e
Stephen Dawes <sdawes@outlook.com>
2016-09-04 20:37:33 +0100

    Setup and styled basic project functionality

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,34 @@
+class ProjectsController < ApplicationController
+  before_action :find_project, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @projects = Project.all.order("created_at desc")
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to @project, notice: "Hellz yeah, Steve! That project was successfully saved!"
+    else
+      render 'new', notice: "Oh no, Steve! I was unable to save your project!"
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def find_project
+    @project = Project.find(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:title, :description, :link)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160827130518) do
+ActiveRecord::Schema.define(version: 20160831153743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,14 @@ ActiveRecord::Schema.define(version: 20160827130518) do
     t.datetime "updated_at", null: false
     t.string   "slug"
     t.index ["slug"], name: "index_posts_on_slug", unique: true, using: :btree
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "link"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
