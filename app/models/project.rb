f3aff6fd48c7a27327e9a5f21e6f68b194daf678
Stephen Dawes <sdawes@outlook.com>
2016-09-22 17:24:05 +0100

    amend styling for paperclip images default sizes and layout

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,17 @@
 class Project < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: [:slugged, :finders]
-  has_attached_file :image, styles: { medium: "600x", thumb: "100x" }
+  has_attached_file :image, styles: {
+  :thumb    => ['100x100#',  :jpg, :quality => 70],
+  :preview  => ['480>',      :jpg, :quality => 70],
+  :large    => ['800>',      :jpg, :quality => 30],
+  :retina   => ['1200>',     :jpg, :quality => 30]
+},
+:convert_options => {
+  :thumb    => '-set colorspace sRGB -strip',
+  :preview  => '-set colorspace sRGB -strip',
+  :large    => '-set colorspace sRGB -strip',
+  :retina   => '-set colorspace sRGB -strip -sharpen 0x0.5'
+}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
